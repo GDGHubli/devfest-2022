ebd20139408a92ce3f6d884db062a5ae199ac5bb
LuckyYada15 <20bcs077@iiitdwd.ac.in>
2022-10-13 01:55:57 +0530

    homepage done

---
+++ src/data/companyData.js
@@ -0,0 +1,44 @@
+const Companydata = [
+    {
+
+
+        company_id: "Job Companies",
+        details: [
+            {
+                com_id: "01",
+                logo: "",
+                compnayName: "Heading-1",
+                content: "This is content for Heading - 1",
+                imageLink: "imageLink-1",
+                redirectLink: "#",
+            },
+            {
+                com_id: "02",
+                logo: "",
+                compnayName: "Heading-2",
+                content: "This is content for Heading - 2",
+                imageLink: "",
+                redirectLink: "#",
+            },
+            {
+                com_id: "03",
+                logo: "",
+                compnayName: "Heading-3",
+                content: "This is content for Heading - 3",
+                imageLink: "",
+                redirectLink: "#",
+            },
+            {
+                com_id: "04",
+                logo: "",
+                compnayName: "Heading-4",
+                content: "THis is content for Heading - 4 ",
+                imageLink: "",
+                redirectLink: "#",
+            },
+        ]
+    }
+]
+
+
+export default Companydata;
